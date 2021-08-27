d5f70864877bdeaaec21a0dc855b8b9f7ceb2e56
Mário André Lehmkuhl de Abreu <mario_andre.l.a@hotmail.com>
2021-08-27 19:38:25 -0300

    definido estrutura inicial do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container grid-lg my-2 py-2">
+    <div class="card mb-2" v-if="listenQuotes.length > 0">
+      <div class="card-header">
+        <div class="h4">Acompanhando</div>
+      </div>
+      <div class="card-body">
+        <WatchListQuotes :listen-quotes="listenQuotes" @unlisten="onUnlisten" />
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+      <div class="card-body">
+        <ListQuotes
+          :quotes="quotes"
+          :listen-quotes="listenQuotes"
+          @listen="onListen"
+          @unlisten="onUnlisten"
+        />
+      </div>
+    </div>
+
+    <!-- <input v-model="model" />
+    <h1>{{ model }}</h1>
+    <input v-model="test" />
+    <h2>{{ quotes }}</h2> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive, ref, toRefs } from "vue";
+import api from "@/services/api";
+import ListQuotes from "./components/ListQuotes.vue";
+import WatchListQuotes from "./components/WatchListQuotes";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ListQuotes, WatchListQuotes },
+  setup() {
+    // ref() - usado pra dar reatividade na variavel
+    let model = ref("Hello World!");
+
+    model.value = "Outra coisa";
+
+    // reactive( - usado para dar reatividade no objeto
+    const data = reactive({
+      quotes: {},
+      listenQuotes: [],
+    });
+
+    onMounted(async () => {
+      const response = await api.all();
+      data.quotes = response.data;
+    });
+
+    function onListen(code) {
+      data.listenQuotes.push(code);
+      // console.log(data);
+    }
+
+    function onUnlisten(code) {
+      data.listenQuotes = data.listenQuotes.filter((key) => key !== code);
+    }
+
+    // api.all().then((response) => {
+    //   data.quotes = response.data;
+    // });
+
+    return {
+      model,
+      ...toRefs(data),
+      onListen,
+      onUnlisten,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
